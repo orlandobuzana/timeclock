4ca275180ccc7d67df5aaba4b0d59c1010cccc5e
Bretton Murphy <djsubaku@gmail.com>
2013-04-28 21:54:45 -0600

    Fix spelling error

--- tsm/tsm/models.py
+++ tsm/tsm/models.py
@@ -9,13 +9,15 @@
 # System Impots
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
-
+from django.core.exceptions import MultipleObjectsReturned
 
 class Project(models.Model):
 
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True) 
 
+    def __unicode__(self):
+        return self.name
 
 class Employee(AbstractBaseUser):
     permissions = (
@@ -32,13 +34,30 @@ class Employee(AbstractBaseUser):
     salary = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=9)
     salaried = models.BooleanField(default=False)
     projects = models.ManyToManyField(Project)
-    premission = models.CharField(choices=permissions, default="EM", max_length=2)
+    permission = models.CharField(choices=permissions, default="EM", max_length=2)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+    USERNAME_FIELD = "username"
 
     def __unicode__(self):
         return self.first_name + " " + self.last_name
+
+
+class ShiftManager(models.Manager):
+    
+    def clocked_in(self, employee):
+        
+        if not isinstance(employee, Employee):
+            raise TypeError("%s is not an object of the type Employee"%str(employee))
+        
+        try:
+            self.get(shift_end=None)
+            return False
+        except Shift.DoesNotExist:
+            return True
+        except MultipleObjectsReturned:
+            raise MultipleObjectsReturned("%s has multiple shifts that are clocked in!"%str(employee))
+
+        return False
 
 
 class Shift(models.Model):
@@ -48,5 +67,9 @@ class Shift(models.Model):
     emploee = models.ForeignKey(Employee)
     project = models.ForeignKey(Project, blank=True, null=True)
     miles = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=6)
+    
+    objects = ShiftManager()
+
+
 
 
