1fe61ad825ff181e2b81a650acc98c89d25490e3
Bretton Murphy <djsubaku@gmail.com>
2013-05-03 08:55:23 -0600

    Add unit tests for clocked_in

--- tsm/tsm/tests.py
+++ tsm/tsm/tests.py
@@ -1,16 +1,67 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+" tsm/tests.py
+" Contributing Authors:
+"    Bretton Murphy (Visgence, Inc)
+"
+" (c) 2012 Visgence, Inc.
 """
 
+# System Imports
 from django.test import TestCase
+from django.core.exceptions import MultipleObjectsReturned
+
+# Local Imports
+from tsm.models import Employee, Shift
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+''''''''''''''''''''''''''''''''''''
+'''         Model Tests          '''
+''''''''''''''''''''''''''''''''''''
+
+
+class ShiftManagerTests(TestCase):
+    fixtures = ['test_employees.json']
+
+    def test_clockedIn_clockedIn(self):
+        ''' A clocked in Employee should get back True '''
+        
+        emp = Employee.objects.get(username="HourlyUser")
+        shift = Shift(employee=emp, shift_start=1367591417)
+        shift.save()
+
+        self.assertTrue(Shift.objects.clocked_in(emp))
+
+    def test_clockedIn_clockedOut(self):
+        ''' A clocked out Employee should get back False '''
+
+        emp = Employee.objects.get(username="HourlyUser")
+        shift = Shift(employee=emp, shift_start=1367591417, shift_end=1367691417)
+        shift.save()
+
+        self.assertFalse(Shift.objects.clocked_in(emp))
+
+    def test_clockedIn_badUser(self):
+        ''' A non Employee object should raise a TypeError '''
+
+        emp = {"user": "fake"}
+        self.assertRaises(TypeError, Shift.objects.clocked_in, emp)
+    
+    def test_clockedIn_multipleClockedIn(self):
+        ''' MultipleObjectsReturned should raise on multiple clocked in shifts found.'''
+
+        emp = Employee.objects.get(username="HourlyUser")
+        shift = Shift(employee=emp, shift_start=1367591417)
+        shift2 = Shift(employee=emp, shift_start=1467591417)
+        shift.save()
+        shift2.save()
+
+        self.assertRaises(MultipleObjectsReturned, Shift.objects.clocked_in, emp)
+
+
+
+
+
+
+
+
