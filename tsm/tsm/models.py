a906bdae48ebe327e449764cb55ee68e6aa18578
Bretton Murphy <djsubaku@gmail.com>
2013-05-03 08:54:57 -0600

    Add hours field to Shift.

--- tsm/tsm/models.py
+++ tsm/tsm/models.py
@@ -14,10 +14,18 @@ from django.core.exceptions import MultipleObjectsReturned
 class Project(models.Model):
 
     name = models.CharField(max_length=50)
-    description = models.TextField(blank=True) 
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.name
+
+
+class EmployeeManager(models.Manager):
+
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
+
 
 class Employee(AbstractBaseUser):
     permissions = (
@@ -45,15 +53,23 @@ class Employee(AbstractBaseUser):
 class ShiftManager(models.Manager):
     
     def clocked_in(self, employee):
-        
+        '''
+            Checks if an Employee is clocked in or not.
+
+            Keyword Arguments:
+                employee - Employee to check clocked in status.
+
+            Return: True if clocked in and False otherwise.
+        '''
+
         if not isinstance(employee, Employee):
             raise TypeError("%s is not an object of the type Employee"%str(employee))
         
         try:
-            self.get(shift_end=None)
-            return False
-        except Shift.DoesNotExist:
+            self.get(shift_end=None, employee=employee)
             return True
+        except Shift.DoesNotExist:
+            return False
         except MultipleObjectsReturned:
             raise MultipleObjectsReturned("%s has multiple shifts that are clocked in!"%str(employee))
 
@@ -64,7 +80,8 @@ class Shift(models.Model):
 
     shift_start = models.IntegerField()
     shift_end = models.IntegerField(null=True, blank=True)
-    emploee = models.ForeignKey(Employee)
+    hours = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=4)
+    employee = models.ForeignKey(Employee)
     project = models.ForeignKey(Project, blank=True, null=True)
     miles = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=6)
     
