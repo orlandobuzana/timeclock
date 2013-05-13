e587047134a9fa649c1a1c063708cd26eaaec7f4
Bretton Murphy <djsubaku@gmail.com>
2013-05-12 20:04:04 -0600

    Update unit tests for clocked_in

--- tsm/tsm/tests.py
+++ tsm/tsm/tests.py
@@ -9,6 +9,8 @@
 # System Imports
 from django.test import TestCase
 from django.core.exceptions import MultipleObjectsReturned
+from django.utils import timezone
+from datetime import timedelta
 
 # Local Imports
 from tsm.models import Employee, Shift
@@ -27,7 +29,7 @@ class ShiftManagerTests(TestCase):
         ''' A clocked in Employee should get back True '''
         
         emp = Employee.objects.get(username="HourlyUser")
-        shift = Shift(employee=emp, shift_start=1367591417)
+        shift = Shift(employee=emp, shift_start=timezone.now())
         shift.save()
 
         self.assertTrue(Shift.objects.clocked_in(emp))
@@ -36,7 +38,9 @@ class ShiftManagerTests(TestCase):
         ''' A clocked out Employee should get back False '''
 
         emp = Employee.objects.get(username="HourlyUser")
-        shift = Shift(employee=emp, shift_start=1367591417, shift_end=1367691417)
+
+        now = timezone.now()
+        shift = Shift(employee=emp, shift_start=now, shift_end=now+timedelta(hours=1))
         shift.save()
 
         self.assertFalse(Shift.objects.clocked_in(emp))
@@ -51,8 +55,8 @@ class ShiftManagerTests(TestCase):
         ''' MultipleObjectsReturned should raise on multiple clocked in shifts found.'''
 
         emp = Employee.objects.get(username="HourlyUser")
-        shift = Shift(employee=emp, shift_start=1367591417)
-        shift2 = Shift(employee=emp, shift_start=1467591417)
+        shift = Shift(employee=emp, shift_start=timezone.now())
+        shift2 = Shift(employee=emp, shift_start=timezone.now())
         shift.save()
         shift2.save()
 
